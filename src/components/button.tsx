087042e0a8d4299399497444b297b5fdaad82d09
Neelam618 <neelamchavan410@gmail.com>
2021-04-05 11:42:23 +0530

    base button text color removed

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -15,7 +15,7 @@ type ButtonTypes = {
   onClick(): void;
 }
 
-const BASE_BUTTON = 'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg text-white'
+const BASE_BUTTON = 'rounded outline-none shadow py-3 px-12 font-normal uppercase tracking-wider text-lg'
 const CONTAINED_BUTTON = `${BASE_BUTTON} bg-red-400 border border-red-400 text-white`
 const OUTLINED_BUTTON = `${BASE_BUTTON} border border-red-400 text-red-400`
 
